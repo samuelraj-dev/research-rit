814968cbd4e93bb0c1bca5d2bdf8bba48d7b7318
samuelraj-dev <samuelraj.dev@outlook.com>
2025-02-02 00:23:04 +0530

    deployment test - fixed server endpoint erros in frontend-2 v3

--- frontend-2/src/libs/constants/server-endpoint.ts
+++ frontend-2/src/libs/constants/server-endpoint.ts
@@ -1,3 +1,3 @@
-export const BASE_API_URL = "http://topoship.com:4200/api";
-export const BASE_URL = "http://topoship.com:4200";
-// export const BASE_URL = "http://localhost:5000/api";
+export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
+  
